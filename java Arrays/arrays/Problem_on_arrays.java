23e6b8af08d7ea99218e6217f6a2a9fcf0016bc7
“joshimalay” <malayjoshi45@gmail.com>
2024-11-28 10:27:25 +0530

    change files

--- java Arrays/arrays/Problem_on_arrays.java
+++ java Arrays/arrays/Problem_on_arrays.java
@@ -17,13 +17,51 @@ public class Problem_on_arrays {
 //             System.out.println("sum = "+sum);
 //         }
 
-        int min = Integer.MAX_VALUE;
-        for(int number:numbers){
-            if(number<min){
-                min = number;
-            }
-        }
-        System.out.println("Number min : "+ min);
+        // int min = Integer.MAX_VALUE;
+        // for(int number:numbers){
+        //     if(number<min){
+        //         min = number;
+        //     }
+        // }
+        // System.out.println("Number min : "+ min);
+
+        // Multi  dimensional array.
+        // row -> student roll number
+        // column -> subject position
+        // int marks[][] = new int[4][3];
+
+        int marks[][] = {
+            {12,98,34},
+            {12,87,67},
+            {23,45,67},
+            {45,34,56},
+        };
+
+        System.out.println(marks[2][1]);
+        System.out.println(marks[2][2]);
+        System.out.println(marks[3][1]);
+
+
+        // marks[0][0] = 12;
+        // marks[0][1] = 98;
+        // marks[0][2] = 34;
+
+        // marks[1][0] = 12;
+        // marks[1][1] = 87;
+        // marks[1][2] = 67;
+
+        // marks[2][0] = 12;
+        // marks[2][1] = 98;
+        // marks[2][2] = 34;
+
+        // marks[3][0] = 12;
+        // marks[3][1] = 87;
+        // marks[3][2] = 67;
+
+
+
+
+
     }
 
 }
