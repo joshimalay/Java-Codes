370e7507742d22387f306f6c35799b2365bfa21e
joshimalay <malayjoshi45@gmail.com>
2024-12-01 18:58:32 +0530

    # New java methods add folder

---
+++ Java Method/learn_methods.java
@@ -0,0 +1,14 @@
+
+public class learn_methods {
+    public static void main(String[] args) {
+        
+    }
+
+    static void greet(){
+        System.out.println("Hello world");
+    }
+
+    // public int average(int a, int b){
+
+    // }
+}
