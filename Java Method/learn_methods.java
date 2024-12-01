edf0162604ba6ea50a3be01f18bb0d27c431b209
joshimalay <malayjoshi45@gmail.com>
2024-12-01 18:59:54 +0530

    # files added

--- Java Method/learn_methods.java
+++ Java Method/learn_methods.java
@@ -1,11 +1,18 @@
 
 public class learn_methods {
     public static void main(String[] args) {
-        
+        System.out.println(1);
+        System.out.println(2);
+        greet();
+        System.out.println(45);
+
+
     }
 
     static void greet(){
+        System.out.println(46);
         System.out.println("Hello world");
+        System.out.println(47);
     }
 
     // public int average(int a, int b){
